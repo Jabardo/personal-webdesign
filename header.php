db2c9b01e6d8c2f39114d22288f0889fc8e011f6
Álvaro Hernández <alvaroherjabardo@hotmail.com>
2014-08-25 15:57:42 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,8 @@
+    <meta charset="utf-8">
+    <title>Alvaro Hernandez | Software Developer and Network Engineer</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href='http://fonts.googleapis.com/css?family=Lobster%7CShadows+Into+Light%7CDroid+Serif%7CChanga+One%7COpen+Sans:400italic,700italic,400,800,700%7CIndie+Flower' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/main.css" type="text/css" media="screen" title="no title"">
+    <link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen" title="no title">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='img/lego.png' rel='shortcut icon' type='image/png'>
