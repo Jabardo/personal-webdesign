6a505e0e61fc20d276cfef4b6e631b043b3b3047
Álvaro Hernández <alvaroherjabardo@hotmail.com>
2014-08-25 15:58:04 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+			<footer>
+      	<a href="http://twitter.com/alv_jabardo" target="_blank"><img src="img/twitter-wrap.png" alt="Twitter Logo" class="social-icon"></a>
+        <a href="https://www.facebook.com/alvaro.hernandezjabardo" target="_blank"><img src="img/facebook-wrap.png" alt="Facebook Logo" class="social-icon"></a>
+        <a href="https://www.linkedin.com/in/alvarohernandezjabardo" target="_blank"><img src="img/linkedin-wrap.png" alt="LinkedIn Logo" class="social-icon"></a>
+        <a href="https://github.com/Jabardo" target="_blank"><img src="img/github-wrap.png" alt="Github Logo" class="social-icon"></a>
+        <p>&copy; <?php echo date("d/m/Y"); ?> Alvaro Hernandez Jabardo.</p>
+      </footer>
+    </div>
+    
+   </body>
+</html> 
