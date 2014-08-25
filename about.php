fc69377967f6bc6f502b2f49eac30af585f693c3
Álvaro Hernández <alvaroherjabardo@hotmail.com>
2014-08-25 15:49:11 +0300

    Update and rename about.html to about.php

---
+++ about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+		<?php include("inc/header.php") ?>  
+	</head>
+  
+  <body>  
+  	<?php include_once("analyticstracking.php") ?>
+  	
+    <header>
+      <a href="index.php" id="logo">
+        <h1>Alvaro Hernandez Jabardo</h1>
+        <h2>Software Developer and Network Engineer</h2>
+      </a>
+      <nav>
+        <ul>
+          <li><a href="portfolio.php">Portfolio</a></li>
+          <li><a href="about.php" class="selected">About</a></li>
+          <li><a href="cv.php">CV</a></li>   
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    
+    <div id="wrapper">
+      <section>
+        <img src="img/alvaro.jpg" alt="Photograph of Alvaro Hernandez Jabardo" class="profile-photo">
+        <h3>About me</h3>
+        <p>Hi, I'm Alvaro! I'm a twenty-five year old software developer and network engineer from Madrid (Spain), currently living in Helsinki (Finland).</p>
+        <p>For in-depth information on my previous work, education and qualifications, please click on the CV tab.</p> 
+        <p>I love everything related with Information and Technology. Here in my personal webpage I show and share my favourite works so far.</p>
+        <p>In my free time I enjoy playing basketball, swimming, listening to Metallica, photography and more.</p>
+        <p>If you would like to follow me on Twitter, my username is <a href="http://twitter.com/alv_jabardo">@alv_jabardo</a>.</p>
+      </section>
+      
+		<?php include("inc/footer.php") ?>
