7369bf822a151dc35705980a9db117ee2f1d032d
Álvaro Hernández <alvaroherjabardo@hotmail.com>
2014-08-25 15:52:03 +0300

    Update and rename contact.html to contact.php

---
+++ contact.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+		<?php include("inc/header.php") ?>
+  </head>
+  
+  <body>  
+  	<?php include_once("analyticstracking.php") ?>
+  	
+    <header>
+      <a href="index.php" id="logo">
+        <h1>Alvaro Hernandez Jabardo</h1>
+        <h2>Software Developer and Network Engineer</h2>
+      </a>
+      <nav>
+        <ul>
+          <li><a href="portfolio.php">Portfolio</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="cv.php">CV</a></li>   
+          <li><a href="contact.php" class="selected">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    
+    <div id="wrapper">
+      <section id="primary">
+        <h3>General Information</h3>
+        <p>I'm looking actively for new professional challenges. Please don't hesitate to contact me if you have any doubts or questions! I will get back to you as soon as possible.</p>
+      </section>
+      
+      <section id="secondary">
+          <h3>Contact Details:</h3>
+          <ul class="contact-info">
+            <li class="phone">Personal phone number: <a href="tel:+358-414-722-616">+358-414-722-616</a></li>
+            <li class="mail">Email: <a href="mailto:alvaroherjabardo@hotmail.com">alvaroherjabardo@hotmail.com</a></li>
+            <li class="twitter">Via Twitter: <a href="http://twitter.com/intent/tweet?screen_name=alv_jabardo">@alv_jabardo</a></li>
+          </ul>
+      </section>
+      
+<?php include("inc/footer.php") ?>
